bfa5c42f6007061dc91f32b4f15927f0ea686ec3
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-04-13 23:22:42 +0000

    fixed button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-function buttonClicked(){
-  // varibles for radius, from user's input 
-  let radius = parseFloat(document.getElementById("radius").value)
+'use strict'
 
+function buttonClicked(){
+  // variables for radius, from user's input 
+  let radius = parseFloat(document.getElementById("radius").value)
+  
   // calculates the volume of a sphere
-  let volume = ()
+  let volume = (3 / 4 * Math.PI * radius ** 3)
 
   // displays the answer
-  document.getElementById('answer').innerHTML = "The volume is: " + volume
+  document.getElementById('answer').innerHTML = "The volume is: " + volume.toFixed(2)
 }
