f3f2c1dd83b976e6d6d649d2806fe59e54a73ca1
Ioana <99274396+Ioana-Mar@users.noreply.github.com>
2022-04-13 22:55:24 +0000

    made website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function buttonClicked(){
+  // varibles for radius, from user's input 
+  let radius = parseFloat(document.getElementById("radius").value)
+
+  // calculates the volume of a sphere
+  let volume = ()
+
+  // displays the answer
+  document.getElementById('answer').innerHTML = "The volume is: " + volume
+}
